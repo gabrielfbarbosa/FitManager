a7d02b163ddb29ab5263896aaa7d30426d6fce39
luna <marcelle@exiti.com.br>
2026-04-13 14:55:58 -0300

    Criar enum EnrollmentStatus com ACTIVE e CANCELLED

---
+++ src/domain/model/enums/EnrollmentStatus.java
@@ -0,0 +1,21 @@
+package domain.model.enums;
+
+public enum EnrollmentStatus {
+    ACTIVE("Ativa"),
+    CANCELLED("Cancelada");
+
+    private final String label;
+
+    EnrollmentStatus(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
+}
